192c9c69e4e95e93a16929665bc3ae72680c512b
shanshanZh0ng <1392106876@qq.com>
2018-07-03 16:56:00 +0800

    second commit

---
+++ src/components/TopTopic.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="topTopic">
+    我是话题
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'topTopic',
+  components: {
+    
+  },
+  data(){
+      return {
+        
+      }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  
+</style>
